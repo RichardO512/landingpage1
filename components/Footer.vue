5c5b3ca7a51023c3e48ad015d28f67a07293042b
Richard Ombrog <ombrog512@gmail.com>
2022-05-13 16:20:45 +0800

    main section

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,68 +1,89 @@
 <template lang="">
   <section>
-    <b-container class="footer d-flex flew-row">
-      <b-row cols="6">
-        <b-col><p class="top-categories">Product</p></b-col>
-        <b-col><p class="top-categories">Company</p></b-col>
-        <b-col><p class="top-categories">Resources</p></b-col>
-        <b-col><p class="top-categories">Use cases</p></b-col>
-        <b-col><p class="top-categories">Social</p></b-col>
-        <b-col><p class="top-categories">Legal</p></b-col>
-        <!-- links -->
-        <b-col><p class="sub-link">Overview</p></b-col>
-        <b-col><p class="sub-link">Aboutt us</p></b-col>
-        <b-col><p class="sub-link">Blog</p></b-col>
-        <b-col><p class="sub-link">Startups</p></b-col>
-        <b-col><p class="sub-link">Twitter</p></b-col>
-        <b-col><p class="sub-link">Terms</p></b-col>
-        <!-- 2 -->
-        <b-col><p class="sub-link">Features</p></b-col>
-        <b-col><p class="sub-link">Careers</p></b-col>
-        <b-col><p class="sub-link">Newsletter</p></b-col>
-        <b-col><p class="sub-link">Enterprise</p></b-col>
-        <b-col><p class="sub-link">Linkedin</p></b-col>
-        <b-col><p class="sub-link">Privacy</p></b-col>
-        <!-- 3 -->
-        <b-col><p class="sub-link">Solution</p></b-col>
-        <b-col><p class="sub-link">Press</p></b-col>
-        <b-col><p class="sub-link">Events</p></b-col>
-        <b-col><p class="sub-link">Government</p></b-col>
-        <b-col><p class="sub-link">Facebook</p></b-col>
-        <b-col><p class="sub-link">Cookies</p></b-col>
-        <!-- 4 -->
-        <b-col><p class="sub-link">Tutorials</p></b-col>
-        <b-col><p class="sub-link">News</p></b-col>
-        <b-col><p class="sub-link">Help centre</p></b-col>
-        <b-col><p class="sub-link">SaaS</p></b-col>
-        <b-col><p class="sub-link">GitHub</p></b-col>
-        <b-col><p class="sub-link">Licenses</p></b-col>
-        <!-- 5 -->
-        <b-col><p class="sub-link">Pricing</p></b-col>
-        <b-col><p class="sub-link">Media kit</p></b-col>
-        <b-col><p class="sub-link">Tutorials</p></b-col>
-        <b-col><p class="sub-link">Marketplaces</p></b-col>
-        <b-col><p class="sub-link">AngelList</p></b-col>
-        <b-col><p class="sub-link">Settings</p></b-col>
-        <!-- 6 -->
-        <b-col><p class="sub-link">Releases</p></b-col>
-        <b-col><p class="sub-link">Contact</p></b-col>
-        <b-col><p class="sub-link">Support</p></b-col>
-        <b-col><p class="sub-link">Ecommerce</p></b-col>
-        <b-col><p class="sub-link">Dribbble</p></b-col>
-        <b-col><p class="sub-link">Contact</p></b-col>
-        <!-- bottom -->
-        <b-col
-          ><p class="bottom-text"><img src="../assets/Untitle.png" /></p
-        ></b-col>
-        <b-col></b-col>
-        <b-col></b-col>
-        <b-col></b-col>
-        <b-col></b-col>
-        <b-col class="bottom-text">
-          2077 Untitled UI. All rights reserved.
-        </b-col>
-      </b-row>
-    </b-container>
+    <footer class="container py-5">
+      <div class="row">
+        <div class="col-6 col-md">
+          <h5 class="font-weight-normal">Product</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Overview</a></li>
+            <li><a class="text-muted" href="#">Feature</a></li>
+            <li><a class="text-muted" href="#">Solutions</a></li>
+            <li><a class="text-muted" href="#">Tutorials</a></li>
+            <li><a class="text-muted" href="#">Pricing</a></li>
+            <li><a class="text-muted" href="#">Releases</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5 class="font-weight-normal">Company</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">About us</a></li>
+            <li><a class="text-muted" href="#">Careers</a></li>
+            <li><a class="text-muted" href="#">Press</a></li>
+            <li><a class="text-muted" href="#">News</a></li>
+            <li><a class="text-muted" href="#">Media kit</a></li>
+            <li><a class="text-muted" href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5 class="font-weight-normal">Resources</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Blog</a></li>
+            <li><a class="text-muted" href="#">Newsletter</a></li>
+            <li><a class="text-muted" href="#">Events</a></li>
+            <li><a class="text-muted" href="#">Help centre</a></li>
+            <li><a class="text-muted" href="#">Tutorials</a></li>
+            <li><a class="text-muted" href="#">Support</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5 class="font-weight-normal">Use cases</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Startups</a></li>
+            <li><a class="text-muted" href="#">Enterprise</a></li>
+            <li><a class="text-muted" href="#">Government</a></li>
+            <li><a class="text-muted" href="#">SaaS</a></li>
+            <li><a class="text-muted" href="#">Marketplaces</a></li>
+            <li><a class="text-muted" href="#">Ecommerce</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5 class="font-weight-normal">Social</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Twitter</a></li>
+            <li><a class="text-muted" href="#">LinkedIn</a></li>
+            <li><a class="text-muted" href="#">Facebook</a></li>
+            <li><a class="text-muted" href="#">GitHub</a></li>
+            <li><a class="text-muted" href="#">AngelList</a></li>
+            <li><a class="text-muted" href="#">Dribbble</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5 class="font-weight-normal">Legal</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Terms</a></li>
+            <li><a class="text-muted" href="#">Privacy</a></li>
+            <li><a class="text-muted" href="#">Cookies</a></li>
+            <li><a class="text-muted" href="#">License</a></li>
+            <li><a class="text-muted" href="#">Settings</a></li>
+            <li><a class="text-muted" href="#">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md">
+            <small class="d-block mb-3 text-muted bottom-text">
+              <p class=""><img src="../assets/Untitle.png" /></p
+            ></small>
+          </div>
+          <div class="col-12 col-md">
+            <small class="d-block mb-3 text-muted bottom-text"
+              >&copy; 2077 Untitled UI. All rights reserved.</small
+            >
+          </div>
+        </div>
+      </div>
+    </footer>
   </section>
 </template>
 <script>
--- components/NewFile.vue
+++ components/NewFile.vue
@@ -1,91 +1,149 @@
 <template>
   <section>
-    <div
-      class="some-page-wrapper d-flex justify-content-center align-items-center"
-    >
+    <footer class="container py-5">
       <div class="row">
-        <div class="column">
-          <div class="orange-column"><img src="../assets/Layer.png" /></div>
+        <div class="col-6 col-md">
+          <h5 class="font-weight-normal">Product</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Overview</a></li>
+            <li><a class="text-muted" href="#">Feature</a></li>
+            <li><a class="text-muted" href="#">Solutions</a></li>
+            <li><a class="text-muted" href="#">Tutorials</a></li>
+            <li><a class="text-muted" href="#">Pricing</a></li>
+            <li><a class="text-muted" href="#">Releases</a></li>
+          </ul>
         </div>
-        <div class="column">
-          <div class="blue-column"><img src="../assets/Sisyphus.png" /></div>
+        <div class="col-6 col-md">
+          <h5 class="font-weight-normal">Company</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">About us</a></li>
+            <li><a class="text-muted" href="#">Careers</a></li>
+            <li><a class="text-muted" href="#">Press</a></li>
+            <li><a class="text-muted" href="#">News</a></li>
+            <li><a class="text-muted" href="#">Media kit</a></li>
+            <li><a class="text-muted" href="#">Contact</a></li>
+          </ul>
         </div>
-        <div class="column">
-          <div class="green-column"><img src="../assets/Circooles.png" /></div>
+        <div class="col-6 col-md">
+          <h5 class="font-weight-normal">Resources</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Blog</a></li>
+            <li><a class="text-muted" href="#">Newsletter</a></li>
+            <li><a class="text-muted" href="#">Events</a></li>
+            <li><a class="text-muted" href="#">Help centre</a></li>
+            <li><a class="text-muted" href="#">Tutorials</a></li>
+            <li><a class="text-muted" href="#">Support</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5 class="font-weight-normal">Use cases</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Startups</a></li>
+            <li><a class="text-muted" href="#">Enterprise</a></li>
+            <li><a class="text-muted" href="#">Government</a></li>
+            <li><a class="text-muted" href="#">SaaS</a></li>
+            <li><a class="text-muted" href="#">Marketplaces</a></li>
+            <li><a class="text-muted" href="#">Ecommerce</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5 class="font-weight-normal">Social</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Twitter</a></li>
+            <li><a class="text-muted" href="#">LinkedIn</a></li>
+            <li><a class="text-muted" href="#">Facebook</a></li>
+            <li><a class="text-muted" href="#">GitHub</a></li>
+            <li><a class="text-muted" href="#">AngelList</a></li>
+            <li><a class="text-muted" href="#">Dribbble</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5 class="font-weight-normal">Legal</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Terms</a></li>
+            <li><a class="text-muted" href="#">Privacy</a></li>
+            <li><a class="text-muted" href="#">Cookies</a></li>
+            <li><a class="text-muted" href="#">License</a></li>
+            <li><a class="text-muted" href="#">Settings</a></li>
+            <li><a class="text-muted" href="#">Contact</a></li>
+          </ul>
         </div>
       </div>
-      <div class="row 2">
-        <div class="column">
-          <div class="green-column"><img src="../assets/Catalog.png" /></div>
-        </div>
-        <div class="column">
-          <div class="orange-column"><img src="../assets/Qoutient.png" /></div>
-        </div>
-        <div class="column">
-          <div class="blue-column"><img src="../assets/Hourglass.png" /></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md">
+            <small class="d-block mb-3 text-muted bottom-text">
+              <p class=""><img src="../assets/Untitle.png" /></p
+            ></small>
+          </div>
+          <div class="col-12 col-md">
+            <small class="d-block mb-3 text-muted bottom-text"
+              >&copy; 2077 Untitled UI. All rights reserved.</small
+            >
+          </div>
         </div>
       </div>
-    </div>
+    </footer>
   </section>
 </template>
 <script>
 export default {
   name: 'NewFile',
+  // data() {
+  //   links: ['']
+  // },
 }
 </script>
 <style scoped>
-.some-page-wrapper {
-  margin: 15px auto;
-  text-align: center;
-}
-.column img {
-  height: 48px;
-  width: 186.99998474121094px;
-}
-
-.row {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  /* width: 100%; */
-  width: 1440px;
+body {
+  font-family: Rubik, Arial, sans-serif;
+  font-weight: 400;
+  font-size: 1rem;
+  overflow-x: hidden;
+  color: #607289;
 }
 
-.column {
-  display: flex;
-  flex-direction: column;
-  flex-basis: 100%;
-  flex: 1;
+h5 {
+  font-size: 1rem;
+  line-height: 1.375;
+  font-family: Rubik, Arial, sans-serif;
+  font-weight: 600;
+  color: #030929;
 }
 
-.blue-column {
-  /* background-color: blue; */
-  /* height: 100px; */
+a {
+  color: #818494;
+  font-weight: 500;
+  text-decoration: none;
+  background-color: transparent;
 }
 
-.green-column {
-  /* background-color: green; */
-  /* height: 100px; */
-}
-@media screen and (min-width: 800px) {
-  .column img {
-    height: 34px;
-    width: 100px;
-  }
-  .double-column img {
-    height: 34px;
-    width: 100px;
-  }
-  .column {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0 auto;
-    flex: 1;
-  }
+a:hover {
+  color: #667085;
 
-  .double-column {
-    flex: 2;
-  }
+  text-decoration: none;
+}
+
+footer {
+  background: #ffffff;
+}
+.bottom-text {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  /* identical to box height, or 150% */
+
+  /* Gray/400 */
+
+  color: #98a2b3;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+  margin: 36px 0px;
 }
 </style>
